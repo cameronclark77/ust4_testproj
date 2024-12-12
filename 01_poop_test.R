ff3e476888d674da6240e1dc39140cd69cf9f087
Clark <C.Clark@exeter.ac.uk>
2024-12-12 11:32:09 +0000

    created test script

---
+++ 01_poop_test.R
@@ -0,0 +1,29 @@
+## -----------------------------------------------------------------------------
+##
+## Script name: 01_poop_test
+##
+## Purpose of script: description
+##
+## Author: Cameron Clark
+##
+## Date Created: 2024-12-12
+##
+## Email: c.clark@exeter.ac.uk
+##
+## -----------------------------------------------------------------------------
+##
+## Notes:
+##
+##
+## -----------------------------------------------------------------------------
+## Load packages and sources ---------------------------------------------------
+{
+library(tidyverse)
+library(janitor)
+if(!require(glue)) {install.packages("glue"); require(glue)}
+library(lubridate)
+}
+## File paths ------------------------------------------------------------------
+
+input_folder  <- "./folder location"
+output_folder <- "./folder location"
